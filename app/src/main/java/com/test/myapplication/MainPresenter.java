70c05bd77def58978de6e13491a08a1b2844aa2c
Fabio Hiroki <fabiothiroki@gmail.com>
2017-03-11 14:55:32 -0300

    Presenter code

--- app/src/main/java/com/test/myapplication/MainPresenter.java
+++ app/src/main/java/com/test/myapplication/MainPresenter.java
@@ -1,8 +1,88 @@
 package com.test.myapplication;
 
+import android.support.annotation.NonNull;
+
+import rx.Observer;
+import rx.Scheduler;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
 /**
  * Created by Indigo on 3/5/17.
  */
 
-public class MainPresenter {
+public class MainPresenter implements MainContract.Presenter {
+
+    @NonNull
+    private CharactersDataSource charactersDataSource;
+
+    @NonNull
+    private Scheduler backgroundScheduler;
+
+    @NonNull
+    private Scheduler mainScheduler;
+
+    @NonNull
+    private CompositeSubscription subscriptions;
+
+    private MainContract.View view;
+
+    public MainPresenter(
+        @NonNull CharactersDataSource charactersDataSource,
+        @NonNull Scheduler backgroundScheduler,
+        @NonNull Scheduler mainScheduler,
+        MainContract.View view) {
+        this.charactersDataSource = charactersDataSource;
+        this.backgroundScheduler = backgroundScheduler;
+        this.mainScheduler = mainScheduler;
+        this.view = view;
+        subscriptions = new CompositeSubscription();
+    }
+
+
+
+    @Override
+    public void loadData() {
+        view.onFetchDataStarted();
+        subscriptions.clear();
+
+        Subscription subscription = charactersDataSource
+                .getCharacters()
+                .subscribeOn(backgroundScheduler)
+                .observeOn(mainScheduler)
+                .subscribe(new Observer<CharactersResponseModel>() {
+                    @Override
+                    public void onCompleted() {
+                        view.onFetchDataCompleted();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        view.onFetchDataError();
+                    }
+
+                    @Override
+                    public void onNext(CharactersResponseModel rootModel) {
+                        view.onFetchDataSuccess(rootModel);
+                    }
+                });
+
+        subscriptions.add(subscription);
+
+    }
+
+    @Override
+    public void subscribe() {
+        loadData();
+    }
+
+    @Override
+    public void unsubscribe() {
+        subscriptions.clear();
+    }
+
+    @Override
+    public void onDestroy() {
+        this.view = null;
+    }
 }
---
+++ app/src/test/java/com/test/myapplication/MainPresenterTest.java
@@ -0,0 +1,8 @@
+package com.test.myapplication;
+
+/**
+ * Created by Indigo on 3/9/17.
+ */
+
+public class MainPresenterTest {
+}
